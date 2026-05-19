02140246b2b54719e40f6dc6de711fd9435b64bd
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-05-19 10:42:12 -0300

    feat: added relationships between designated tables

--- exercicio_3/relationships.sql
+++ exercicio_3/relationships.sql
@@ -1,1 +1,9 @@
---relationships
+--categories 1,1..1,N products
+ALTER TABLE exercicio3.t_products	
+ADD CONSTRAINT exercicio3_t_products_fk_categorie_id
+FOREIGN KEY (categorie_id) REFERENCES exercicio3.t_categories (id);
+
+--users 1,1...0,n
+ALTER TABLE exercicio3.t_orders	
+ADD CONSTRAINT exercicio3_t_orders_fk_user_id
+FOREIGN KEY (user_id) REFERENCES exercicio3.t_user (id);
