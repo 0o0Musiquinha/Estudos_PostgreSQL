9de69a949da3f05619b28776677c782d75022c4f
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-05-22 15:27:42 -0300

    fix: calling custom_types without specifying the schema

--- exercicio_4/modelo_fisico/tables.sql
+++ exercicio_4/modelo_fisico/tables.sql
@@ -1,13 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS btree_gist;
 --Guardião
 CREATE TABLE IF NOT EXISTS exercicio4.t_guardian(
-	id 				BIGINT 				NOT NULL,
-	name 			VARCHAR(120) 		NOT NULL,
-	cpf 			VARCHAR(11)  		NOT NULL,
-	email 			VARCHAR(254) 		NOT NULL,
-	phonenumber 	VARCHAR(11)  		NOT NULL,
-	status 			guardian_status 	NOT NULL,
-	created_at 		TIMESTAMPTZ 		NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0)
+	id 				BIGINT 						NOT NULL,
+	name 			VARCHAR(120) 				NOT NULL,
+	cpf 			VARCHAR(11)  				NOT NULL,
+	email 			VARCHAR(254) 				NOT NULL,
+	phonenumber 	VARCHAR(11)  				NOT NULL,
+	status 			exercicio4.guardian_status 	NOT NULL,
+	created_at 		TIMESTAMPTZ 				NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0)
 );
 
 CREATE TABLE IF NOT EXISTS exercicio4.t_species(
@@ -34,27 +34,27 @@ CREATE TABLE IF NOT EXISTS exercicio4.t_race(
 );
 
 CREATE TABLE IF NOT EXISTS exercicio4.t_veterinarian(
-	id 		       	BIGINT 				NOT NULL,
-	name 	       	VARCHAR(120) 		NOT NULL,
-	cpf 	       	VARCHAR(11) 		NOT NULL,
-	crmv 	       	VARCHAR(9) 			NOT NULL,
-	admission_date 	TIMESTAMPTZ 		NOT NULL,
-	status 		   	veterinarian_status NOT NULL 	DEFAULT 'ativo',
-	created_at 	   	TIMESTAMPTZ 		NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0),
+	id 		       	BIGINT 							NOT NULL,
+	name 	       	VARCHAR(120) 					NOT NULL,
+	cpf 	       	VARCHAR(11) 					NOT NULL,
+	crmv 	       	VARCHAR(9) 						NOT NULL,
+	admission_date 	TIMESTAMPTZ 					NOT NULL,
+	status 		   	exercicio4.veterinarian_status 	NOT NULL 	DEFAULT 'ativo',
+	created_at 	   	TIMESTAMPTZ 					NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0),
 
 	--FKs
-	specialty_id INT NOT NULL
+	specialty_id 	INT 							NOT NULL
 
 );
 
 CREATE TABLE IF NOT EXISTS exercicio4.t_animal(
-	id 			BIGINT 			NOT NULL,
-	name 		VARCHAR(120) 	NOT NULL,
-	birthday 	DATE 			NOT NULL,
-	weight 		DECIMAL(5,2) 	NOT NULL,
-	sex 		animal_sex 		NOT NULL,
-	status 		animal_status 	NOT NULL 	DEFAULT 'ativo',
-	created_at 	TIMESTAMPTZ 	NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0),
+	id 			BIGINT 						NOT NULL,
+	name 		VARCHAR(120) 				NOT NULL,
+	birthday 	DATE 						NOT NULL,
+	weight 		DECIMAL(5,2) 				NOT NULL,
+	sex 		exercicio4.animal_sex 		NOT NULL,
+	status 		exercicio4.animal_status 	NOT NULL 	DEFAULT 'ativo',
+	created_at 	TIMESTAMPTZ 				NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0),
 
 	--FKs
 	race_id 	INT 			NULL,
@@ -64,16 +64,16 @@ CREATE TABLE IF NOT EXISTS exercicio4.t_animal(
 );
 
 CREATE TABLE IF NOT EXISTS exercicio4.t_appointment(
-	id 				BIGINT 				NOT NULL,
-	duration 		TSTZRANGE 			NOT NULL,
-	diagnosis 		VARCHAR(200) 		NOT NULL,
-	price 			DECIMAL(7,2) 		NOT NULL,
-	status 			appointment_status  NOT NULL,
-	created_at 		TIMESTAMPTZ 		NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0),
+	id 				BIGINT 							NOT NULL,
+	duration 		TSTZRANGE 						NOT NULL,
+	diagnosis 		VARCHAR(200) 					NOT NULL,
+	price 			DECIMAL(7,2) 					NOT NULL,
+	status 			exercicio4.appointment_status  	NOT NULL,
+	created_at 		TIMESTAMPTZ 					NOT NULL 	DEFAULT CURRENT_TIMESTAMP(0),
 
 	--FKs
-	animal_id 		BIGINT 				NOT NULL,
-	veterinarian_id BIGINT 				NOT NULL,
+	animal_id 		BIGINT 							NOT NULL,
+	veterinarian_id BIGINT 							NOT NULL,
 
 	--EXCLUDE
 	CONSTRAINT exercicio4_t_appointment_ex_duration_c_animal_id EXCLUDE 
