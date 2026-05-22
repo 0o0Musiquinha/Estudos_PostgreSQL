595c835401e05dffef23a5e95e4f7303c05cb5b1
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-05-22 10:56:01 -0300

    refactor: changed table 'user' to 'users' and columns 'creation_date' to 'created_at'

--- exercicio_3/tables.sql
+++ exercicio_3/tables.sql
@@ -1,49 +1,49 @@
 -- Caso necessário
 --DROP TABLE IF EXISTS exercicio3.t_orders;
 --DROP TABLE IF EXISTS exercicio3.t_products ;
---DROP TABLE IF EXISTS exercicio3.t_user;
+--DROP TABLE IF EXISTS exercicio3.t_users;
 --DROP TABLE IF EXISTS exercicio3.t_categories;
 
 --Usúario
-CREATE TABLE IF NOT EXISTS exercicio3.t_user(
+CREATE TABLE IF NOT EXISTS exercicio3.t_users(
 	LIKE exercicio2.t_user INCLUDING ALL
 );
 
 --Categorias
 CREATE TABLE IF NOT EXISTS exercicio3.t_categories(
-	id INT NOT NULL, --Demarquei como 'INT'pois, no meu entendimento, categoria abrange uma quantidade muito menor de dados que serão inseridos
-	name VARCHAR(60) NOT NULL,
-	status VARCHAR(9) NOT NULL,
-	creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP(0)
+	id 			INT 			NOT NULL,
+	name 		VARCHAR(60) 	NOT NULL,
+	status 		VARCHAR(9) 		NOT NULL,
+	created_at 	TIMESTAMPTZ 	DEFAULT CURRENT_TIMESTAMP(0)
 	
 );
 
 --Produtos
 CREATE TABLE IF NOT EXISTS exercicio3.t_products(
-	id BIGINT NOT NULL,
-	code VARCHAR(19) NOT NULL DEFAULT exercicio3.product_code_generator(),
-	name VARCHAR(120) NOT NULL,
-	description VARCHAR(300) NOT NULL,
-	price DECIMAL(6,2) NOT NULL, --Anotar sobre problemas do REAL e FLOAT
-	stock INT NOT NULL,
-	status VARCHAR(12) NOT NULL,
-	creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP(0),
+	id 				BIGINT 			NOT NULL,
+	code 			VARCHAR(19) 	NOT NULL DEFAULT exercicio3.product_code_generator(),
+	name 			VARCHAR(120) 	NOT NULL,
+	description 	VARCHAR(300) 	NOT NULL,
+	price 			DECIMAL(6,2) 	NOT NULL, --Anotar sobre problemas do REAL e FLOAT
+	stock 			INT 			NOT NULL,
+	status 			VARCHAR(12) 	NOT NULL,
+	created_at 		TIMESTAMPTZ 	DEFAULT CURRENT_TIMESTAMP(0),
 	
 	--FOREIGN KEYs
-	categorie_id INT NOT NULL
+	categorie_id 	INT 			NOT NULL
 	
 );
 
 --Pedidos
 CREATE TABLE IF NOT EXISTS exercicio3.t_orders(
-	id BIGINT NOT NULL,
-	code VARCHAR(28) NOT NULL DEFAULT exercicio3.order_code_generator(),
-	total_value DECIMAL(6,2) NOT NULL,
-	status VARCHAR(20)NOT NULL,
-	order_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+	id 				BIGINT 			NOT NULL,
+	code 			VARCHAR(28) 	NOT NULL DEFAULT exercicio3.order_code_generator(),
+	total_value 	DECIMAL(6,2) 	NOT NULL,
+	status 			VARCHAR(20) 	NOT NULL,
+	order_date 		TIMESTAMPTZ 	NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
 	--FOREIGN KEYs
-	user_id BIGINT NOT NULL
+	user_id 		BIGINT 			NOT NULL
 	
 );
 
