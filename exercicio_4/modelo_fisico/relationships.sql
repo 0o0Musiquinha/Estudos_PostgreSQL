861f9899cc39d0eb1485018bcbf99beb5205d558
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-05-22 12:03:01 -0300

    style: improved relationships and sequences legibility

--- exercicio_4/modelo_fisico/relationships.sql
+++ exercicio_4/modelo_fisico/relationships.sql
@@ -1,11 +1,35 @@
 --PRIMARY KEYS
-	ALTER TABLE exercicio4.t_race ADD CONSTRAINT exercicio4_t_race_fk_species_id FOREIGN KEY(species_id) REFERENCES exercicio4.t_species(id);
+	ALTER TABLE exercicio4.t_race 
+		ADD CONSTRAINT exercicio4_t_race_fk_species_id 
+		FOREIGN KEY(species_id) 
+			REFERENCES exercicio4.t_species(id);
 	
-	ALTER TABLE exercicio4.t_animal ADD CONSTRAINT exercicio4_t_animal_fk_race_id FOREIGN KEY(race_id) REFERENCES exercicio4.t_race(id);
-	ALTER TABLE exercicio4.t_animal ADD CONSTRAINT exercicio4_t_animal_fk_species_id FOREIGN KEY(species_id) REFERENCES exercicio4.t_species(id);
-	ALTER TABLE exercicio4.t_animal ADD CONSTRAINT exercicio4_t_animal_fk_guardian_id FOREIGN KEY(guardian_id) REFERENCES exercicio4.t_guardian(id);
-	
-	ALTER TABLE exercicio4.t_veterinarian ADD CONSTRAINT exercicio4_t_veterinarian_fk_specialty_id FOREIGN KEY(specialty_id) REFERENCES exercicio4.t_specialty(id);
+	ALTER TABLE exercicio4.t_animal 
+		ADD CONSTRAINT exercicio4_t_animal_fk_race_id 
+		FOREIGN KEY(race_id) 
+			REFERENCES exercicio4.t_race(id);
 
-	ALTER TABLE exercicio4.t_appointment ADD CONSTRAINT exercicio4_t_appointment_fk_animal_id FOREIGN KEY(animal_id) REFERENCES exercicio4.t_animal(id);
-	ALTER TABLE exercicio4.t_appointment ADD CONSTRAINT exercicio4_t_appointment_fk_veterinarian_id FOREIGN KEY(veterinarian_id) REFERENCES exercicio4.t_veterinarian(id);
+	ALTER TABLE exercicio4.t_animal 
+		ADD CONSTRAINT exercicio4_t_animal_fk_species_id 
+			FOREIGN KEY(species_id) 
+			REFERENCES exercicio4.t_species(id);
+
+	ALTER TABLE exercicio4.t_animal 
+		ADD CONSTRAINT exercicio4_t_animal_fk_guardian_id 
+			FOREIGN KEY(guardian_id) 
+			REFERENCES exercicio4.t_guardian(id);
+	
+	ALTER TABLE exercicio4.t_veterinarian 
+		ADD CONSTRAINT exercicio4_t_veterinarian_fk_specialty_id 
+			FOREIGN KEY(specialty_id) 
+			REFERENCES exercicio4.t_specialty(id);
+
+	ALTER TABLE exercicio4.t_appointment 
+		ADD CONSTRAINT exercicio4_t_appointment_fk_animal_id 
+			FOREIGN KEY(animal_id) 
+			REFERENCES exercicio4.t_animal(id);
+			
+	ALTER TABLE exercicio4.t_appointment 
+		ADD CONSTRAINT exercicio4_t_appointment_fk_veterinarian_id 
+			FOREIGN KEY(veterinarian_id) 
+			REFERENCES exercicio4.t_veterinarian(id);
--- exercicio_4/modelo_fisico/sequences.sql
+++ exercicio_4/modelo_fisico/sequences.sql
@@ -15,7 +15,9 @@ CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_guardian_id_seq
 	OWNED BY exercicio4.t_guardian.id;
 
 	--Define como padrão para a tabela
-	ALTER TABLE exercicio4.t_guardian ALTER COLUMN id SET DEFAULT nextval('exercicio4.exercicio4_t_guardian_id_seq');
+	ALTER TABLE exercicio4.t_guardian 
+		ALTER COLUMN id 
+			SET DEFAULT nextval('exercicio4.exercicio4_t_guardian_id_seq');
 	
 --Categorias
 CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_species_id_seq
@@ -25,7 +27,9 @@ CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_species_id_seq
 	OWNED BY exercicio4.t_species.id;
 
 	--Define como padrão para a tabela
-	ALTER TABLE exercicio4.t_species ALTER COLUMN id SET DEFAULT nextval('exercicio4.exercicio4_t_species_id_seq');
+	ALTER TABLE exercicio4.t_species 
+		ALTER COLUMN id 
+			SET DEFAULT nextval('exercicio4.exercicio4_t_species_id_seq');
 
 --Produtos 
 CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_race_id_seq
@@ -35,7 +39,9 @@ CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_race_id_seq
 	OWNED BY exercicio4.t_race.id;
 
 	--Define como padrão para a tabela
-	ALTER TABLE exercicio4.t_race ALTER COLUMN id SET DEFAULT nextval('exercicio4.exercicio4_t_race_id_seq');
+	ALTER TABLE exercicio4.t_race 
+		ALTER COLUMN id 
+			SET DEFAULT nextval('exercicio4.exercicio4_t_race_id_seq');
 
 --Pedidos
 CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_race_id_seq
@@ -45,7 +51,9 @@ CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_race_id_seq
 	OWNED BY exercicio4.t_race.id;
 
 	--Define como padrão para a tabela
-	ALTER TABLE exercicio4.t_race ALTER COLUMN id SET DEFAULT nextval('exercicio4.exercicio4_t_race_id_seq');
+	ALTER TABLE exercicio4.t_race 
+		ALTER COLUMN id 
+			SET DEFAULT nextval('exercicio4.exercicio4_t_race_id_seq');
 
 CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_veterinarian_id_seq
 	AS BIGINT
@@ -54,7 +62,9 @@ CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_veterinarian_id_seq
 	OWNED BY exercicio4.t_veterinarian.id;
 
 	--Define como padrão para a tabela
-	ALTER TABLE exercicio4.t_veterinarian ALTER COLUMN id SET DEFAULT nextval('exercicio4.exercicio4_t_veterinarian_id_seq');
+	ALTER TABLE exercicio4.t_veterinarian 
+		ALTER COLUMN id 
+			SET DEFAULT nextval('exercicio4.exercicio4_t_veterinarian_id_seq');
 
 CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_animal_id_seq
 	AS BIGINT
@@ -63,7 +73,9 @@ CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_animal_id_seq
 	OWNED BY exercicio4.t_animal.id;
 
 	--Define como padrão para a tabela
-	ALTER TABLE exercicio4.t_animal ALTER COLUMN id SET DEFAULT nextval('exercicio4.exercicio4_t_animal_id_seq');
+	ALTER TABLE exercicio4.t_animal 
+		ALTER COLUMN id 
+			SET DEFAULT nextval('exercicio4.exercicio4_t_animal_id_seq');
 
 CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_appointment_id_seq
 	AS BIGINT
@@ -72,4 +84,6 @@ CREATE SEQUENCE IF NOT EXISTS exercicio4.exercicio4_t_appointment_id_seq
 	OWNED BY exercicio4.t_appointment.id;
 
 	--Define como padrão para a tabela
-	ALTER TABLE exercicio4.t_appointment ALTER COLUMN id SET DEFAULT nextval('exercicio4.exercicio4_t_appointment_id_seq');
+	ALTER TABLE exercicio4.t_appointment 
+		ALTER COLUMN id 
+			SET DEFAULT nextval('exercicio4.exercicio4_t_appointment_id_seq');
