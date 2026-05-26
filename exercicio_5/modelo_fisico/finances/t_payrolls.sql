0f8547bbb890d8d938d1990dc12b1735d96c7a64
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-05-26 13:44:36 -0300

    feat: constraints for table in 'finances ' squema

--- exercicio_5/modelo_fisico/finances/t_payrolls.sql
+++ exercicio_5/modelo_fisico/finances/t_payrolls.sql
@@ -4,10 +4,24 @@ CREATE TABLE IF NOT EXISTS finances.t_payrolls(
     payment_day     TIMESTAMPTZ     NOT NULL,
     gross_salary    DECIMAL(8,2)    NOT NULL,
     deduction       DECIMAL(8,2)    NOT NULL,
-    net_salary      DECIMAL(8,2)    NOT NULL,
+    net_salary      DECIMAL(8,2)    NOT NULL -- possibilidade: GENERATED ALWAYS AS (gross_salary - deduction),
     status          payroll_status  NOT NULL,
     created_at      TIMESTAMPTZ     NOT NULL,
 
     --FK
     employee_id     INT             NOT NULL
 );
+
+ALTER TABLE finances.t_payrolls
+    ADD CONSTRAINT finances_t_payrolls_pk 
+    PRIMARY KEY(id);
+
+ALTER TABLE finances.t_payrolls
+    ADD CONSTRAINT finances_t_payrolls_ck_employee_title
+    CHECK(
+        name ~* '^[^ ]{1}(?!.*  )[a-záàâãèéêìíîóòôôúùû ]{1,}[^ ]{1}$'
+    );
+
+ALTER TABLE finances.t_payrolls
+        ALTER COLUMN created_at
+            SET DEFAULT CURRENT_TIMESTAMP(0);
--- exercicio_5/modelo_fisico/finances/t_reimbursements.sql
+++ exercicio_5/modelo_fisico/finances/t_reimbursements.sql
@@ -3,4 +3,19 @@ CREATE TABLE IF NOT EXISTS finances.t_reimbursements (
     value DECIMAL(8,2) NOT NULL,
     description VARCHAR(2000) NOT NULL,
     status reimbursement_status NOT NULL
+
+    --FK
+    employee_id INT NOT NULL,
+    approver_id INT NOT NULL,
+    department_id SMALLINT NOT NULL
 );
+
+ALTER TABLE finances.t_reimbursements
+    ADD CONSTRAINT finances_t_reimbursements_pk 
+    PRIMARY KEY(id);
+
+ALTER TABLE finances.t_reimbursements
+    ADD CONSTRAINT finances_t_reimbursements_ck_employee_id
+    CHECK(
+        employee_id != approver_id
+    );
