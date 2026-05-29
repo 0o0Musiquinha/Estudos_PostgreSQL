46ed5617f3426c0eb721cab02d0e2ec55002ff15
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-05-29 13:53:55 -0300

    test: added test validation for inserts in table 'exercicio5_hr.t_positions'

---
+++ tests/exercicio_5/inserts/t_positions.test.sql
@@ -0,0 +1,37 @@
+BEGIN;
+
+SET search_path TO extension_pgtap;
+SELECT plan(2); --Quantidade de testesa a serem realizados
+
+SELECT throws_ok(
+    $$
+        INSERT INTO exercicio5_hr.t_positions(
+            name,
+            description
+        )
+        VALUES (
+            'Augusto Rodolfo de K4lheir0s'
+            'teste dos testes testados pelos magos dos testes'
+        );
+    $$,
+    '23514',
+    'new row for relation "t_positions" violates check constraint "exercicio5_hr_t_positions_ck_name"'
+);
+
+SELECT throws_ok(
+    $$
+        INSERT INTO exercicio5_hr.t_positions(
+            name,
+            description
+        )
+        VALUES (
+            'Augusto Rodolfo de Kalheiros'
+            'teste dos testes testados pelos  magos dos testes'
+        );
+    $$,
+    '23514',
+    'new row for relation "t_positions" violates check constraint "exercicio5_hr_t_positions_ck_description"'
+);
+SELECT * FROM finish();
+
+ROLLBACK;
