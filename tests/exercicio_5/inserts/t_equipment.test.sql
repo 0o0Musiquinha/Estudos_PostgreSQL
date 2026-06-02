185d9c83be326bc4d4504e7bbf09385a4ada65fb
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-06-02 15:05:13 -0300

    refactor: changed test inputs

--- tests/exercicio_5/inserts/t_equipment.test.sql
+++ tests/exercicio_5/inserts/t_equipment.test.sql
@@ -28,7 +28,7 @@ SELECT throws_ok(
         )
         VALUES (
             'clovis45',
-            'r4938henk#',
+            'r4938#enk',
             'R7 7800x3d',
             '2026-05-25 15:30:00-03',
             120.30,
